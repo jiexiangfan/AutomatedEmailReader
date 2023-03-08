f94eaf59714e5afbaba75e01ce0ae372c3487468
Jie Fan <jxiangfan@gmail.com>
2023-03-08 16:46:52 +1100

    fancy animated background

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const boxes = () => {
+    let array = [],
+      numberOfBox = 15;
+    for (let i = 0; i < numberOfBox; i++) array.push(i);
+    return array;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="area">
+        <ul className="circles">
+          {boxes().map((i) => (
+            <li key={i} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
